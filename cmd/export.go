7e6fcc0eb705a3cfd2c3e92af3af1aa2e5e474d3
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2026-03-29 20:31:28 +0200

    chore: replace stub with real filesystem

--- cmd/export.go
+++ cmd/export.go
@@ -4,24 +4,12 @@ import (
 	"fmt"
 
 	"github.com/antoniopantaleo/wwdc/internal/adapters/exporter"
+	"github.com/antoniopantaleo/wwdc/internal/adapters/filesystem"
 	"github.com/antoniopantaleo/wwdc/internal/adapters/scraper"
 	"github.com/antoniopantaleo/wwdc/internal/domain"
 	"github.com/antoniopantaleo/wwdc/internal/usecases"
 	"github.com/spf13/cobra"
 )
-
-type stubFileSystem struct {
-	makeDirFunc   func(path string) error
-	writeFileFunc func(path string, data []byte) error
-}
-
-func (s *stubFileSystem) MakeDir(path string) error {
-	return s.makeDirFunc(path)
-}
-
-func (s *stubFileSystem) WriteFile(path string, data []byte) error {
-	return s.writeFileFunc(path, data)
-}
 
 func NewExportCommand() *cobra.Command {
 	var format string
@@ -42,35 +30,49 @@ func NewExportCommand() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			scraper := &scraper.StubScraper{
 				Events: []domain.WWDCEvent{
+			{
+				Title: "WWDC24",
+				Year:  2024,
+				CoverURL: "https://example.com/wwdc24.jpg",
+				Videos: []domain.WWDCVideo{
 					{
-						Title: "WWDC26",
-						Year: 2026,
-						CoverURL: "https://example.com/wwdc26.jpg",
-						Videos: []domain.WWDCVideo{
-							{
-								Title: "Session 1",
-								VideoURL: "https://example.com/session1.mp4",
-								Content: "This is the content of session 1.",
-							},
-						},
+						Title:    "Session 1",
+						VideoURL: "https://example.com/session1.mp4",
+						Content:  "This is the content of session 1.",
+					},	
+					{
+						Title: "Session 2",
+						VideoURL: "https://example.com/session2.mp4",
+						Content:  "This is the content of session 2.",
 					},
 				},
+			},
+
+			{
+				Title: "WWDC23",
+				Year:  2023,
+				CoverURL: "https://example.com/wwdc23.jpg",
+				Videos: []domain.WWDCVideo{
+					{
+						Title:    "Session 3",
+						VideoURL: "https://example.com/session3.mp4",
+						Content:  "This is the content of session 3.",
+					},
+					{
+						Title: "Session 4",
+						VideoURL: "https://example.com/session4.mp4",
+						Content:  "This is the content of session 4.",
+					},
+				},
+			},
+		},
 			}
 			var exp domain.WWDCExporter
 			switch format {
 			case "json":
 				exp = exporter.NewJSONExporter(cmd.OutOrStdout())
 			case "markdown", "md":
-				fs := &stubFileSystem{
-					makeDirFunc: func(path string) error {
-						fmt.Printf("MakeDir called with path: %s\n", path)
-						return nil
-					},
-					writeFileFunc: func(path string, data []byte) error {
-						fmt.Printf("WriteFile called with path: %s and data: %s\n", path, string(data))
-						return nil
-					},
-				}
+				fs := filesystem.NewOSFileSystem("./WWDC")
 				exp = exporter.NewMarkdownExporter(fs)
 			default:
 				return fmt.Errorf("unsupported format: %s", format)
