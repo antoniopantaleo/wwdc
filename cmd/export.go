9ce38b076fb59569f32123298901355fd8fa1fda
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2026-03-30 12:07:15 +0200

    feat: split format options in subcommands, add output flag to JSON and remove omitTitle from it

--- cmd/export.go
+++ cmd/export.go
@@ -1,50 +1,68 @@
 package cmd
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/antoniopantaleo/wwdc/internal/adapters/exporter"
-	"github.com/antoniopantaleo/wwdc/internal/domain"
 	"github.com/antoniopantaleo/wwdc/internal/usecases"
 	"github.com/spf13/cobra"
 )
 
 func NewExportCommand(d *dependencies) *cobra.Command {
-	var format string
-	var omitTitle bool
 	cmd := &cobra.Command{
 		Use: "export",
 		Short: "Scrape and export WWDC session videos",
 		Long: `
   Scrape WWDC session videos from the Apple Developer website and export them in the specified format.
+`,
+	}
 
-  Supported formats are:
-  - json: Exports the data as JSON to standard output.
-  - markdown|md: Exports the data as Markdown files, creating a directory for each year and a Markdown file for each session video.
-`,
-		Example: `
-  wwdc export --format json
-  wwdc export -f md
-`,
+	cmd.AddCommand(newJSONExportCommand(d))
+	cmd.AddCommand(newMarkdownExportCommand(d))
+
+	return cmd
+}
+
+func newJSONExportCommand(d *dependencies) *cobra.Command {
+	var outputPath string
+	cmd := &cobra.Command{
+		Use: "json",
+		Short: "Short description",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			scraper := d.Scraper
-			var exp domain.WWDCExporter
-			switch format {
-			case "json":
-				exp = exporter.NewJSONExporter(cmd.OutOrStdout(), omitTitle)
-			case "markdown", "md":
-				fs := d.FileSystem
-				exp = exporter.NewMarkdownExporter(fs, omitTitle)
-			default:
-				return fmt.Errorf("unsupported format: %s", format)
+			writer := cmd.OutOrStdout()
+			if outputPath != "" {
+				f, err := os.Create(outputPath)
+				if err != nil {
+					return err
+				}
+				defer f.Close()
+				writer = f
 			}
-			usecase := usecases.NewScrapeAndExportUseCase(scraper, exp)
+			exporter := exporter.NewJSONExporter(writer)
+			usecase := usecases.NewScrapeAndExportUseCase(scraper, exporter)
 			return usecase.Execute()
 		},
 	}
-	cmd.Flags().StringVarP(&format, "format", "f", "", "Export format. Currently supported formats are: json, markdown (or md).")
-	cmd.MarkFlagRequired("format")
 
+	cmd.Flags().StringVarP(&outputPath, "output", "o", "", "Output path")
+	return cmd
+}
+
+func newMarkdownExportCommand(d *dependencies) *cobra.Command {
+	var omitTitle bool
+	cmd := &cobra.Command{
+		Use: "markdown",
+		Short: "Short description",
+		Aliases: []string{"md"},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			scraper := d.Scraper
+			fs := d.FileSystem
+			exporter := exporter.NewMarkdownExporter(fs, omitTitle)
+			usecase := usecases.NewScrapeAndExportUseCase(scraper, exporter)
+			return usecase.Execute()
+		},
+	}
 	cmd.Flags().BoolVar(&omitTitle, "omit-title", false, "Do not write title")
 	return cmd
 }
--- internal/adapters/exporter/json.go
+++ internal/adapters/exporter/json.go
@@ -15,7 +15,7 @@ type JSONWWDCEvent struct {
 }
 
 type JSONWWDCVideo struct {
-	Title    string `json:"title,omitempty"`
+	Title    string `json:"title"`
 	VideoURL string `json:"videoUrl"`
 	Content  string `json:"content"`
 }
@@ -26,17 +26,16 @@ type jsonExportData struct {
 
 type JSONExporter struct {
 	writer io.Writer
-	omitTitle bool
 }
 
-func NewJSONExporter(writer io.Writer, omitTitle bool) *JSONExporter {
-	return &JSONExporter{writer: writer, omitTitle: omitTitle}
+func NewJSONExporter(writer io.Writer) *JSONExporter {
+	return &JSONExporter{writer: writer}
 }
 
 func (e *JSONExporter) Export(events []domain.WWDCEvent) error {
 	jsonEvents := make([]JSONWWDCEvent, len(events))
 	for i, event := range events {
-		jsonEvents[i] = convertToJSONWWDCEvent(event, e.omitTitle)
+		jsonEvents[i] = convertToJSONWWDCEvent(event)
 	}
 	exportData := jsonExportData{
 		Events: jsonEvents,
@@ -49,10 +48,10 @@ func (e *JSONExporter) Export(events []domain.WWDCEvent) error {
 	return nil
 }
 
-func convertToJSONWWDCEvent(event domain.WWDCEvent, omitTitle bool) JSONWWDCEvent {
+func convertToJSONWWDCEvent(event domain.WWDCEvent) JSONWWDCEvent {
 	jsonVideos := make([]JSONWWDCVideo, len(event.Videos))
 	for i, video := range event.Videos {
-		jsonVideos[i] = convertToJSONWWDCVideo(video, omitTitle)
+		jsonVideos[i] = convertToJSONWWDCVideo(video)
 	}
 	return JSONWWDCEvent{
 		Title:    event.Title,
@@ -62,13 +61,9 @@ func convertToJSONWWDCEvent(event domain.WWDCEvent, omitTitle bool) JSONWWDCEven
 	}
 }
 
-func convertToJSONWWDCVideo(video domain.WWDCVideo, omitTitle bool) JSONWWDCVideo {
-	title := video.Title
-	if omitTitle {
-		title = ""
-	}
+func convertToJSONWWDCVideo(video domain.WWDCVideo) JSONWWDCVideo {
 	return JSONWWDCVideo{
-		Title:    title,
+		Title:    video.Title,
 		VideoURL: video.VideoURL,
 		Content:  video.Content,
 	}
