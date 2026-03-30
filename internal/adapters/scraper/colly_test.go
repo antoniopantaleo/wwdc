3f275b9dd43aa483e01970cb0019a96c77e51c01
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2026-03-30 16:56:48 +0200

    test(colly): improve test obtaining exact instance of event and video

--- internal/adapters/scraper/colly_test.go
+++ internal/adapters/scraper/colly_test.go
@@ -5,7 +5,10 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"slices"
 	"testing"
+
+	"github.com/antoniopantaleo/wwdc/internal/domain"
 )
 
 func TestCollyScraperEventsList(t *testing.T) {
@@ -21,14 +24,22 @@ func TestCollyScraperEventsList(t *testing.T) {
 	if len(events) != 6 {
 		t.Fatalf("expected 6 events, got %d", len(events))
 	}
-	for _, event := range events {
-		for _, video := range event.Videos {
-			if video.Title != "Bring your Live Activity to Apple Watch" {
-				t.Fatalf("expected Bring your Live Activity to Apple Watch title, got %s instead", video.Title)
-			}
-			if video.VideoURL != "https://devstreaming-cdn.apple.com/videos/wwdc/2024/10068/4/C621DA91-3F64-481C-8D10-25A5C5FCD587/downloads/wwdc2024-10068_hd.mp4?dl=1" {
-			t.Fatalf("expected https://devstreaming-cdn.apple.com/videos/wwdc/2024/10068/4/C621DA91-3F64-481C-8D10-25A5C5FCD587/downloads/wwdc2024-10068_hd.mp4?dl=1 videoURL, got %s instead", video.VideoURL)
-		}
-		}
+	wwdc2024Idx := slices.IndexFunc(events, func(e domain.WWDCEvent) bool {
+		return e.Year == 2024
+	})
+	if wwdc2024Idx == -1 || len(events) <= wwdc2024Idx {
+		t.Fatal("unable to find wwdc2024 event")
 	}
+	wwdc2024 := events[wwdc2024Idx]
+	appleWatchVideoIdx := slices.IndexFunc(wwdc2024.Videos, func (v domain.WWDCVideo) bool {
+		return v.Title == "Bring your Live Activity to Apple Watch"
+	})
+	if appleWatchVideoIdx == -1 || len(wwdc2024.Videos) <= appleWatchVideoIdx {
+		t.Fatal("unable to find Apple Watch video")
+	}
+	appleWatchVideo := wwdc2024.Videos[appleWatchVideoIdx]
+	if appleWatchVideo.VideoURL != "https://devstreaming-cdn.apple.com/videos/wwdc/2024/10068/4/C621DA91-3F64-481C-8D10-25A5C5FCD587/downloads/wwdc2024-10068_hd.mp4?dl=1" {
+			t.Fatalf("expected https://devstreaming-cdn.apple.com/videos/wwdc/2024/10068/4/C621DA91-3F64-481C-8D10-25A5C5FCD587/downloads/wwdc2024-10068_hd.mp4?dl=1 videoURL, got %s instead", appleWatchVideo.VideoURL)
+		}
+	
 }
